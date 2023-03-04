9e8df3bca138e27568ddb386903002942c8446a4
himanshu9999git <hchimanshu9999@gmail.com>
2023-03-04 23:04:23 +0530

    App Complete

---
+++ src/components/Image.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MediaHub from "../assets/MediaHub.png";
+
+const Image = () => {
+  return (
+    <>
+      <div>
+        <img src={MediaHub} alt="MediaHub" height={70}/>
+      </div>
+    </>
+  );
+};
+
+export default Image;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogoMH from "../assets/LogoMH.png";
+
+const Logo = () => {
+    return (
+        <>
+          <div>
+            <img src={LogoMH} alt="Logo" height={45}/>
+          </div>
+        </>
+      );
+}
+
+export default Logo
